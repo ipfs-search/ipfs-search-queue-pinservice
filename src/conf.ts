0d71f0a5c627c5aa837b1dc47fe478551bbd88f4
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2022-10-26 10:54:10 +0100

    Sensible defaults.
    * Disable debugging.
    * Default to 1 worker.

--- src/conf.ts
+++ src/conf.ts
@@ -1,8 +1,7 @@
-import { cpus } from "node:os";
 import { env } from "node:process";
 
 export const ENVIRONMENT = env.NODE_ENV || "development";
-export const PROCESSES = parseInt(env.PROCESSES) || cpus().length;
+export const PROCESSES = parseInt(env.PROCESSES) || 1;
 export const HOST = env.PINSERVICE_HOST || "localhost";
 export const PORT = parseInt(env.PINSERVICE_PORT || "7070");
 export const QUEUE_HOST = env.AMQP_URL || "amqp://guest:guest@localhost:5672";
