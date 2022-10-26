e281ed366b2e3dada1470a37ec64037c2514ed51
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2022-10-26 11:23:29 +0100

    Simplify & clarify delegate stuff.

--- src/conf.ts
+++ src/conf.ts
@@ -7,13 +7,9 @@ export const PORT = parseInt(env.PINSERVICE_PORT || "7070");
 export const QUEUE_HOST = env.AMQP_URL || "amqp://guest:guest@localhost:5672";
 export const LOG_NAMESPACE = "queue-pinservice";
 
-let delegates;
-const delegates_err =
-  "PINSERVICE_DELEGATES should be a JSON array with one or more delegates, e.g. [\"/ip4/161.230.143.49/tcp/64885/p2p/12D3KooWCfksHSx489oMAH2ysfNTvVtzQLj4u5PHfrXckYNzUU4x\"]. Consider running: PINSERVICE_DELEGATES=`ipfs id | jq -r -c '.Addresses'` npm start";
-try {
-  delegates = JSON.parse(env.PINSERVICE_DELEGATES);
-  if (!Array.isArray(delegates)) throw new Error(delegates_err);
-} catch (e) {
-  throw new Error(delegates_err);
-}
-export const DELEGATES = delegates;
+export const DELEGATES = JSON.parse(env.PINSERVICE_DELEGATES);
+if (!Array.isArray(DELEGATES))
+  throw new Error(
+    'PINSERVICE_DELEGATES should be a JSON array with one or more delegates, e.g. ["/ip4/161.230.143.49/tcp/64885/p2p/12D3KooWCfksHSx489oMAH2ysfNTvVtzQLj4u5PHfrXckYNzUU4x"].' +
+      "Consider running: PINSERVICE_DELEGATES=`ipfs id | jq -r -c '.Addresses'` npm start"
+  );
