fa4b917e905ee21e3a744d63ca98b542211a6305
Frido Emans <verderster@gmail.com>
2022-10-26 14:49:57 +0200

    slice amount of delegates to max 20

--- src/conf.ts
+++ src/conf.ts
@@ -1,15 +1,22 @@
+import { cpus } from "node:os";
 import { env } from "node:process";
 
 export const ENVIRONMENT = env.NODE_ENV || "development";
-export const PROCESSES = parseInt(env.PROCESSES) || 1;
+export const PROCESSES = parseInt(env.PROCESSES || '') || 1
 export const HOST = env.PINSERVICE_HOST || "localhost";
 export const PORT = parseInt(env.PINSERVICE_PORT || "7070");
 export const QUEUE_HOST = env.AMQP_URL || "amqp://guest:guest@localhost:5672";
 export const LOG_NAMESPACE = "queue-pinservice";
-
-export const DELEGATES = JSON.parse(env.PINSERVICE_DELEGATES);
-if (!Array.isArray(DELEGATES))
-  throw new Error(
-    'PINSERVICE_DELEGATES should be a JSON array with one or more delegates, e.g. ["/ip4/161.230.143.49/tcp/64885/p2p/12D3KooWCfksHSx489oMAH2ysfNTvVtzQLj4u5PHfrXckYNzUU4x"].' +
-      "Consider running: PINSERVICE_DELEGATES=`ipfs id | jq -r -c '.Addresses'` npm start"
-  );
+// set delegates of your ipfs node(s) e.g. using PINSERVICE_DELEGATES=ipfs id | jq -r -c '.Addresses'
+// note that when not providing any delegates, the service will still work, but the client can give
+// an error about the request
+// Allowed formats for PINSERVICE_DELEGATES:
+// string, comma-separated string, JSON array
+let delegates;
+try {
+  delegates = JSON.parse(env.PINSERVICE_DELEGATES);
+  if (!Array.isArray(delegates)) throw new Error();
+} catch (e) {
+  delegates = env.PINSERVICE_DELEGATES?.split(",");
+}
+export const DELEGATES = delegates.slice(0,20) || [""];
